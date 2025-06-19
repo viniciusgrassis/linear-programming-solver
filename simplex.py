52afe84e9a9d3c194a88898bb3e7bb8878a56b54
Vinicius Assis <vini1904@hotmail.com>
2025-06-19 17:24:12 -0300

    adicionado simplex

---
+++ simplex.py
@@ -0,0 +1,78 @@
+import numpy as np
+from carregar_entrada import carregar_entrada
+from criar_tableau import criar_tableau
+
+def coluna_pivo(linha_obj):
+    valores = linha_obj[1:-1]
+    maior = np.max(valores)
+
+    if maior > 0:
+        coluna_pivo = np.argmax(valores) + 1
+        return coluna_pivo
+    else:
+        return -1
+
+def linha_pivo(tableau, coluna_pivo):
+    valores = tableau[1:, coluna_pivo]
+    b = tableau[1:, -1]
+
+    if np.all(valores <= 0):
+        return -1
+    
+    razoes = np.divide(b, valores, out=np.full_like(b, np.inf), where=valores > 0)
+
+    linha_pivo = np.argmin(razoes) + 1
+
+    if razoes[linha_pivo - 1] != np.inf:
+        return linha_pivo
+    else:
+        return None
+
+def pivoteamento(tableau, linha_pivo, coluna_pivo):
+    pivo = tableau[linha_pivo, coluna_pivo]
+    tableau[linha_pivo] /= pivo
+
+    num_linhas, num_colunas = tableau.shape
+
+    for i in range(num_linhas):
+        if i != linha_pivo:
+            tableau[i] -= tableau[linha_pivo] * tableau[i, coluna_pivo]
+    
+    return tableau
+
+def simplex(tableau_inicial):
+    tableau = tableau_inicial.copy()
+    num_iteracoes = 1
+
+    while True:
+
+        linha_obj = tableau[0]
+        indice_coluna_pivo = coluna_pivo(linha_obj)
+
+        if indice_coluna_pivo == -1: #solução ótima
+            break       
+
+        indice_linha_pivo = linha_pivo(tableau, indice_coluna_pivo)
+        
+        if indice_linha_pivo is None:
+            print("Solucoes ilimitadas.")
+            break
+                 
+        tableau = pivoteamento(tableau, indice_linha_pivo, indice_coluna_pivo)
+        num_iteracoes += 1
+
+    return tableau
+
+if __name__ == "__main__":
+    c, a, r, b = carregar_entrada("modelo.txt")
+    if a is not None:
+        tableau_inicial = criar_tableau(c, a, r, b)
+        print("Tableau inicial:")
+        print(tableau_inicial)
+        tableau_final = simplex(tableau_inicial)
+        print("Tableau final:")
+        print(tableau_final)
+    else:
+        print("Erro ao carregar.")
+        exit(1)
+    
