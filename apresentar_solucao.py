cfcbfd29c753628f30ee89c8c690af08c627ebb6
Vinicius Assis <vini1904@hotmail.com>
2025-06-21 13:51:08 -0300

    att f

---
+++ apresentar_solucao.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def apresentar_solucao(tableau_final, num_var, num_sol = 1):
+    try:
+        solucao_str = ""
+        if num_sol > 1:
+            solucao_str += f"\n---/ Solução ótima alternativa {num_sol - 1} /---"
+        else:
+            solucao_str += "\n---/ Solução ótima encontrada /---"
+
+        z_otimo = tableau_final[0, -1]*-1
+        solucao_str += f"\nValor ótimo para Z = {z_otimo:.2f}"
+        solucao_str += "---/ Variáveis básicas /---\n"
+
+        for i in range(num_var):
+            coluna_x = tableau_final[:, i + 1]
+            valor = 0.0
+
+            if np.sum(np.round(coluna_x, 4)) == 1 and np.count_nonzero(np.round(coluna_x, 4)) == 1:
+                linha_base = np.where(np.round(coluna_x, 4) == 1)[0][0]
+                valor = tableau_final[linha_base, -1]
+
+            solucao_str += f"\n x{i + 1} = {valor:.2f}"
+
+        print(solucao_str)
+        return True
+
+    except Exception as e:
+        print(f"Erro ao apresentar a solução: {e}")
+        return False
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from big_m_create import big_m_create
 from big_m_checker import big_m_checker
 from simplex import simplex, linha_pivo, pivoteamento
 from multi_solution_checker import multi_solution_checker
+from apresentar_solucao import apresentar_solucao
 
 np.set_printoptions(
     suppress=True,      
@@ -18,8 +19,6 @@ if __name__ == "__main__":
         tableau_inicial = None
         if np.any(r == 0) or np.any(r == 2):
             tableau_inicial = big_m_create(c, a, r, b)
-            print("Tableau inicial com Big M:")
-            print(tableau_inicial)
             tableau_final = simplex(tableau_inicial)
             teste = big_m_checker(tableau_final, r, a)
             if not teste:
@@ -27,23 +26,18 @@ if __name__ == "__main__":
                 exit(1)
             else:
                 print("Solução viável encontrada.")
-                print("Tableau final:")
-                print(tableau_final)
+                apresentar_solucao(tableau_final, a.shape[1], num_sol=1)
         else:
             tableau_inicial = criar_tableau(c, a, r, b)
-            print("Tableau inicial:")
-            print(tableau_inicial)
             tableau_final = simplex(tableau_inicial)
-            print("Tableau final:")
-            print(tableau_final)
+            apresentar_solucao(tableau_final, a.shape[1], num_sol=1)
 
         teste_coluna = multi_solution_checker(tableau_final)
         if teste_coluna is not None:
             print("Múltiplas soluções:")
             linha_alternativa = linha_pivo(tableau_final, teste_coluna)
             tableau_alternativo = pivoteamento(tableau_final, linha_alternativa, teste_coluna)
-            print("Tableau alternativo:")
-            print(tableau_alternativo)
+            apresentar_solucao(tableau_final, a.shape[1], num_sol=2)
     else:
         print("Erro ao carregar.")
         exit(1)
